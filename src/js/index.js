782d942fd4d55ff17706085a4e009c35f5d899c1
pjw9412 <pjwts9412@gmail.com>
2022-03-06 15:31:30 +0900

    trying to deploy

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,15 @@ http.createServer((req, res) => {
 }).listen(8888, () => {
     console.log('8888번 포트에서 서버 대기 중입니다!');
 });
+
+// footer - playbackpannel
+const footer = document.querySelector('.playbackPannel');
+
+// footer에 들어오는 모든 클릭 이벤트를 감지
+footer.addEventListener('click', (e) => {
+    // green-toggle해야하는 6개의 버튼이 클릭되었을 경우. toggle한다.
+    if (e.target.matches('.fa-solid--green-off') || e.target.matches('.fa-solid--green')) {
+        e.target.classList.toggle('fa-solid--green-off');
+        e.target.classList.toggle('fa-solid--green');
+    }
+});
