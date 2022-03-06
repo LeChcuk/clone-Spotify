4cde48572fb9d3dce06a877f81f21b2881d12579
pjw9412 <pjwts9412@gmail.com>
2022-03-06 16:44:04 +0900

    1

--- src/js/index.js
+++ src/js/index.js
@@ -2,24 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 1234);
 
 app.get('/', (req, res) => {
     res.send('Hello Woooorld');
 });
 
 app.listen(app.get('port'), () => {
-    console.log('3000번 포트에서 서버 대기 중입니다!');
+    console.log('1234번 포트에서 서버 대기 중입니다!');
 });
 
-// footer - playbackpannel
-const footer = document.querySelector('.playbackPannel');
+// // footer - playbackpannel
+// const footer = document.querySelector('.playbackPannel');
 
-// footer에 들어오는 모든 클릭 이벤트를 감지
-footer.addEventListener('click', (e) => {
-    // green-toggle해야하는 6개의 버튼이 클릭되었을 경우. toggle한다.
-    if (e.target.matches('.fa-solid--green-off') || e.target.matches('.fa-solid--green')) {
-        e.target.classList.toggle('fa-solid--green-off');
-        e.target.classList.toggle('fa-solid--green');
-    }
-});
+// // footer에 들어오는 모든 클릭 이벤트를 감지
+// footer.addEventListener('click', (e) => {
+//     // green-toggle해야하는 6개의 버튼이 클릭되었을 경우. toggle한다.
+//     if (e.target.matches('.fa-solid--green-off') || e.target.matches('.fa-solid--green')) {
+//         e.target.classList.toggle('fa-solid--green-off');
+//         e.target.classList.toggle('fa-solid--green');
+//     }
+// });
